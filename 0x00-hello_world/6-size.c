f03edfa4f21a791c9292835faf66e2da21b62a6a
Elhadidym <mohamedkedh@gmail.com>
2023-09-10 10:26:05 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        printf("size of a char: %u byte(s) \n");
+        return (0);
+}
