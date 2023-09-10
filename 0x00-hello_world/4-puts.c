0346cd63f560de3c969141b4a2f8fca30bd7fab8
Elhadidym <mohamedkedh@gmail.com>
2023-09-09 18:44:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <studio.h>
+
 /**
  * main -Entry point
  *
- * Description: A C program that prints with function
+ * Description: A C program that prints with put function
  *
  * Return: Alwayes 0 (Success)
 */
